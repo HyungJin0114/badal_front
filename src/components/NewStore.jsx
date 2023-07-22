bb07c540f9c9211b1324ae87238e77da303485c5
Nam HyunJin <skaguswls200@naver.com>
2023-07-22 20:06:55 +0900

    feat: store put api 수정, formData.append name 변경
    feat: store put api 수정, formData.append name 변경

--- src/components/NewStore.jsx
+++ src/components/NewStore.jsx
@@ -15,7 +15,7 @@ export default function NewStore({ requestType }) {
     // const newStoreData = { name, location, storePhoneNumber, category, img };
     try {
       const formData = new FormData();
-      formData.append('menuName', name);
+      formData.append('name', name);
       formData.append('location', location);
       formData.append('storePhoneNumber', storePhoneNumber);
       formData.append('image', image);
@@ -42,12 +42,12 @@ export default function NewStore({ requestType }) {
     // const newStoreData = { name, location, storePhoneNumber, category, img };
     try {
       const formData = new FormData();
-      formData.append('menuName', name);
+      formData.append('name', name);
       formData.append('location', location);
       formData.append('storePhoneNumber', storePhoneNumber);
       formData.append('image', image);
 
-      await axios.put(`${process.env.REACT_APP_API_SERVERURL}/api/stores/`, formData, {
+      await axios.put(`${process.env.REACT_APP_API_SERVERURL}/api/stores/${myStore.id}`, formData, {
         withCredentials: true,
         headers: {
           'Content-Type': 'multipart/form-data',
@@ -71,9 +71,21 @@ export default function NewStore({ requestType }) {
       <h1 className="font-bold text-center mx-auto w-fit text-xl">가게 {requestType ? '생성하기' : '변경하기'}</h1>
       <input type="text" placeholder="이름" value={name} required onChange={(e) => setName(e.target.value)} />
       <input type="text" placeholder="위치" value={location} required onChange={(e) => setLocation(e.target.value)} />
-      <input type="text" placeholder="전화번호" value={storePhoneNumber} required onChange={(e) => setStorePhoneNumber(e.target.value)} />
-      <input type="text" placeholder="카테고리" value={category} required onChange={(e) => setCategory(e.target.value)} />
-      <input type="file" placeholder="사진" value={image} required onChange={(e) => setImage(e.target.files[0])} />
+      <input
+        type="text"
+        placeholder="전화번호"
+        value={storePhoneNumber}
+        required
+        onChange={(e) => setStorePhoneNumber(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="카테고리"
+        value={category}
+        required
+        onChange={(e) => setCategory(e.target.value)}
+      />
+      <input type="file" placeholder="사진" required onChange={(e) => setImage(e.target.files[0])} />
       <Button
         onClick={
           requestType
@@ -86,7 +98,9 @@ export default function NewStore({ requestType }) {
         }
         text={requestType ? '생성하기' : '변경하기'}
       />
-      {!requestType && <Button onClick={onClickDelBtn} text={'삭제'} />}
+      {requestType && <Button onClick={onClickDelBtn} text={'삭제'} />}
     </div>
   );
 }
+
+//requestType 넘어올 때 변경하기 같이 해야하고, storeId값 받아와야함 append안에 name으로 받아야하고
