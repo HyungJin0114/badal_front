ccb652ac4802ef992c6dcf8e01024c86ec02a518
jangdu <jjd0324@gmail.com>
2023-07-22 21:37:22 +0900

    feat: 가게들 get요청 시 쿼리스트링으로 태그 불러오기

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import CategoryCard from './CategoryCard';
 
-export default function Category() {
+export default function Category({ setCategory }) {
   const count = ['전체', '양식', '일식', '한식', '중식'];
   return (
     <div className="flex flex-row my-3 mx-auto overflow-hidden">
       {count.map((item) => {
         return (
           <div className="" key={item}>
-            <CategoryCard category={item} />
+            <CategoryCard category={item} setCategory={setCategory} />
           </div>
         );
       })}
